52f8167e8e3b9b07a5b7632dad8138f65d2762d8
tiendunggg99 <tiendung_ms@outlook.com>
2022-09-01 16:21:16 +0700

    Add project files.

---
+++ tiendung99.Core.Entity/IRepository/IRepositoryBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiendung99.Core.Entity.IRepository
+{
+    public interface IRepositoryBase<TEntity,TModel> where TEntity : class, new() where TModel : class
+    {
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> expression);
+        Task<IEnumerable<TEntity>> PagingAsync(Expression<Func<TEntity, bool>> expression,int? page, int? pageSize);
+        Task UpdateRangeAsync(IEnumerable<TModel> models);
+        Task DeleteRangeAsync(IEnumerable<TModel> models);
+        Task AddRangeAsync(IEnumerable<TModel> models);
+        Task SaveAsync();
+
+
+    }
+}
---
+++ tiendung99.Core.Entity/Repository/RepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiendung99.Core.Entity.Repository
+{
+    internal class RepositoryBase
+    {
+    }
+}
