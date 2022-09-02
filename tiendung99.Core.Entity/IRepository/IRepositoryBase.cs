23074e07fcb934afb31b14daa04f7c662bdccca6
tiendunggg99 <tiendung_ms@outlook.com>
2022-09-02 09:54:52 +0700

    Add Generic Repository

--- tiendung99.Core.Entity/IRepository/IRepositoryBase.cs
+++ tiendung99.Core.Entity/IRepository/IRepositoryBase.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
 namespace tiendung99.Core.Entity.IRepository
 {
-    public interface IRepositoryBase<TEntity,TModel> where TEntity : class, new() where TModel : class
+    public interface IRepositoryBase<TEntity, TModel> where TEntity : class, new() where TModel : class
     {
-        Task CreateAsync(TEntity entity);
-        Task UpdateAsync(TEntity entity);
-        Task DeleteAsync(TEntity entity);
-        Task<IEnumerable<TEntity>> GetAllAsync();
-        Task<TEntity> GetByIdAsync(int id);
-        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> expression);
-        Task<IEnumerable<TEntity>> PagingAsync(Expression<Func<TEntity, bool>> expression,int? page, int? pageSize);
+        Task AddAsync(TModel model);
+        Task UpdateAsync(TModel model);
+        Task DeleteAsync(TModel model);
+        Task<IEnumerable<TModel>> GetAllAsync();
+        Task<TModel> GetByIdAsync(Guid id);
+        Task<IEnumerable<TModel>> FindAsync(Expression<Func<TModel, bool>> expression);
+        Task<IEnumerable<TModel>> PagingAsync(Expression<Func<TModel, bool>> expression, int page, int pageSize);
         Task UpdateRangeAsync(IEnumerable<TModel> models);
         Task DeleteRangeAsync(IEnumerable<TModel> models);
         Task AddRangeAsync(IEnumerable<TModel> models);
+        Task<bool> CheckExist(TModel model);
         Task SaveAsync();
-
-
     }
 }
--- tiendung99.Core.Entity/Repository/RepositoryBase.cs
+++ tiendung99.Core.Entity/Repository/RepositoryBase.cs
@@ -1,12 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using tiendung99.Core.Entity.IRepository;
 
 namespace tiendung99.Core.Entity.Repository
 {
-    internal class RepositoryBase
+    public class RepositoryBase<TEntity, TModel> : IRepositoryBase<TEntity, TModel> where TEntity : class, new() where TModel : class
+
     {
+        private readonly IMapper _mapper;
+        private readonly DbContext _context;
+        public RepositoryBase(IMapper mapper, DbContext dbContext)
+        {
+            _mapper = mapper;
+            _context = dbContext;
+        }
+
+        public async Task AddAsync(TModel model)
+        {
+            var entity = _mapper.Map<TEntity>(model);
+            await _context.Set<TEntity>().AddAsync(entity);
+            throw new NotImplementedException();
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TModel> models)
+        {
+            var entities = _mapper.Map<IEnumerable<TEntity>>(models);
+            await _context.Set<TEntity>().AddRangeAsync(entities);
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> CheckExist(TModel model)
+        {
+            var entity = _mapper.Map<TEntity>(model);
+            if (await _context.Set<TEntity>().FindAsync(entity) != null)
+            {
+                return true;
+            }
+            return false;
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TModel model)
+        {
+            var entity = _mapper.Map<TEntity>(model);
+            _context.Set<TEntity>().Remove(entity);
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRangeAsync(IEnumerable<TModel> models)
+        {
+            var entities = _mapper.Map<IEnumerable<TEntity>>(models);
+            _context.Set<TEntity>().RemoveRange(entities);
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TModel>> FindAsync(Expression<Func<TModel, bool>> expressionModel)
+        {
+            var expression = _mapper.Map<Expression<Func<TEntity, bool>>>(expressionModel);
+            var entities = await _context.Set<TEntity>().Where(expression).ToListAsync();
+            return _mapper.Map<IEnumerable<TModel>>(entities);
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TModel>> GetAllAsync()
+        {
+            var entities = await _context.Set<TEntity>().ToListAsync();
+            return _mapper.Map<IEnumerable<TModel>>(entities);
+            throw new NotImplementedException();
+        }
+
+        public async Task<TModel> GetByIdAsync(Guid id)
+        {
+            var entity = await _context.Set<TModel>().FindAsync(id);
+            return _mapper.Map<TModel>(entity);
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TModel>> PagingAsync(Expression<Func<TModel, bool>> expression, int page, int pageSize)
+        {
+            var emtities = await _context.Set<TModel>().Where(expression).Skip(page * pageSize).Take(pageSize).ToListAsync();
+            return _mapper.Map<IEnumerable<TModel>>(emtities);
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TModel model)
+        {
+            var entity = _mapper.Map<TEntity>(model);
+            _context.Set<TEntity>().Update(entity);
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRangeAsync(IEnumerable<TModel> models)
+        {
+            var entities = _mapper.Map<IEnumerable<TEntity>>(models);
+            _context.UpdateRange(entities);
+            throw new NotImplementedException();
+        }
     }
 }
